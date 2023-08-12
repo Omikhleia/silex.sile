a5299645baa7553e56206a874852dfe882d01be3
Omikhleia <didier.willis@gmail.com>
2023-08-12 14:41:30 +0200

    feat: Extend AST utilities with some more convenience methods
    Especially useful when processing some XML schemas with structure
    oriented markup.

--- silex/ast.lua
+++ silex/ast.lua
@@ -78,8 +78,8 @@ function ast.createStructuredCommand (command, options, content, position)
   return result
 end
 
---- Extract the sub-content tree from a command node,
---- that is the child nodes of the command node.
+--- Extract the sub-content tree from a (command) node,
+--- that is the child nodes of the (command) node.
 ---@param content   table   AST tree
 ---@return          table   AST tree
 function ast.subContent (content)
@@ -88,6 +88,61 @@ function ast.subContent (content)
     out[#out+1] = val
   end
   return out
+end
+
+-- String trimming
+local function trimLeft (str)
+  return str:gsub("^%s*", "")
+end
+local function trimRight (str)
+  return str:gsub("%s*$", "")
+end
+
+--- Content tree trimming: remove leading and trailing spaces, but from
+--- a content tree i.e. possibly containing several elements.
+---@param content   table   AST tree
+---@return          table   AST tree
+function ast.trimSubContent (content)
+  if #content == 0 then
+    return
+  end
+  if type(content[1]) == "string" then
+    content[1] = trimLeft(content[1])
+    if content[1] == "" then
+      table.remove(content, 1)
+    end
+  end
+  if type(content[#content]) == "string" then
+    content[#content] = trimRight(content[#content])
+    if content[#content] == "" then
+      table.remove(content, #content)
+    end
+  end
+  return content
+end
+
+--- Process the AST walking through content nodes as a "structure":
+--- Text nodes are ignored (e.g. usually just spaces due to XML indentation)
+--- Command nodes are enriched with their "true" node position, so we can later
+--- refer to it (as with an XPath pos()).
+---@param content   table   AST tree
+function ast.processAsStructure (content)
+  local iElem = 0
+  local nElem = 0
+  for i = 1, #content do
+    if type(content[i]) == "table" then
+      nElem = nElem + 1
+    end
+  end
+  for i = 1, #content do
+    if type(content[i]) == "table" then
+      iElem = iElem + 1
+      content[i].options._pos_ = iElem
+      content[i].options._last_ = iElem == nElem
+      SILE.process({ content[i] })
+    end
+    -- All text nodes in ignored in structure tags.
+  end
 end
 
 return ast
