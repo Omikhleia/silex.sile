c0f97d843fd4e0d6cb51e88e4f8aa532b630324e
Omikhleia <didier.willis@gmail.com>
2023-08-14 17:26:16 +0200

    feat: Emphasis nesting and re-implemented center and ragged envs

--- silex/fixes.lua
+++ silex/fixes.lua
@@ -1,13 +1,18 @@
 --- Stuff that should eventually be in SILE core in my opinion,
 --- but isn't yet...
 
--- Annoyingly, inputters options can be nil
+-- -----------------------------------------------------------------------
+
+-- Annoyingly, inputters options can be nil.
+-- Let be more tolerant and safe
 local inputter = require("inputters.base")
 local oldInit = inputter._init
 function inputter:_init (options)
   oldInit(self, options)
   self.options = self.options or {}
 end
+
+-- -----------------------------------------------------------------------
 
 -- Greek numbering ("greek") for counters, similar to "alpha".
 -- There are books where one wants to number items with Greek letters in
@@ -31,4 +36,115 @@ SU.formatNumber.und.greek = function(num)
     SU.error("Greek numbering is only supported up to 24")
   end
   return out
+end
+
+-- -----------------------------------------------------------------------
+
+local class = require("classes.plain")
+local oldreg = class.registerCommands
+function class:registerCommands()
+  oldreg(self)
+
+  -- Italic nesting.
+  -- See https://github.com/sile-typesetter/sile/issues/1048
+  -- The proposal there was to keep "em" and introduced "emph" as the nesting
+  -- variant. But let's be more drastic and just make "em" nestable.
+  -- We shouldn't care for compatibility with early defective designs.
+  self:registerCommand("em", function (_, content)
+    local style = SILE.settings:get("font.style")
+    local toggle = (style and style:lower() == "italic") and "Regular" or "Italic"
+    SILE.call("font", { style = toggle }, content)
+  end)
+
+  -- SILE's original centered and ragged environments do not allow nesting,
+  -- i.e. they reset the left and/or right skips and thus apply to the full
+  -- line width, loosing all margins.
+  -- Say you have some nice indented block (margins on both sides), and you
+  -- "center" something in it, then that centering goes past the block margins.
+  -- That might be considered to be a "feature", but frankly, most people won't
+  -- expect it.
+  -- Paragraph indentation is another concern, it ought to be preserved in
+  -- ragged left or right environments, but not in centered ones.
+  -- Let's go for a full proper reimplementation of these environments.
+  self:registerCommand("center", function (_, content)
+    if #SILE.typesetter.state.nodes ~= 0 then
+      SU.warn("\\center environment started after other nodes in a paragraph, may not center as expected")
+    end
+    SILE.settings:temporarily(function ()
+      local lskip = SILE.settings:get("document.lskip") or SILE.nodefactory.glue()
+      local rskip = SILE.settings:get("document.rskip") or SILE.nodefactory.glue()
+      SILE.settings:set("document.parindent", SILE.nodefactory.glue())
+      SILE.settings:set("current.parindent", SILE.nodefactory.glue())
+      SILE.settings:set("document.lskip", SILE.nodefactory.hfillglue(lskip.width.length))
+      SILE.settings:set("document.rskip", SILE.nodefactory.hfillglue(rskip.width.length))
+      SILE.settings:set("typesetter.parfillskip", SILE.nodefactory.glue())
+      SILE.settings:set("document.spaceskip", SILE.length("1spc", 0, 0))
+      SILE.process(content)
+      SILE.call("par")
+    end)
+  end, "Typeset its contents in a centered block (keeping margins).")
+
+  self:registerCommand("raggedright", function (_, content)
+    SILE.settings:temporarily(function ()
+      local lskip = SILE.settings:get("document.lskip") or SILE.nodefactory.glue()
+      local rskip = SILE.settings:get("document.rskip") or SILE.nodefactory.glue()
+      SILE.settings:set("document.lskip", SILE.nodefactory.glue(lskip.width.length))
+      SILE.settings:set("document.rskip", SILE.nodefactory.hfillglue(rskip.width.length))
+      SILE.settings:set("typesetter.parfillskip", SILE.nodefactory.glue())
+      SILE.settings:set("document.spaceskip", SILE.length("1spc", 0, 0))
+      SILE.process(content)
+      SILE.call("par")
+    end)
+  end, "Typeset its contents in a left aligned block (keeping margins).")
+
+  self:registerCommand("raggedleft", function (_, content)
+    SILE.settings:temporarily(function ()
+      local lskip = SILE.settings:get("document.lskip") or SILE.nodefactory.glue()
+      local rskip = SILE.settings:get("document.rskip") or SILE.nodefactory.glue()
+      SILE.settings:set("document.lskip", SILE.nodefactory.hfillglue(lskip.width.length))
+      SILE.settings:set("document.rskip", SILE.nodefactory.glue(rskip.width.length))
+      SILE.settings:set("typesetter.parfillskip", SILE.nodefactory.glue())
+      SILE.settings:set("document.spaceskip", SILE.length("1spc", 0, 0))
+      SILE.process(content)
+      SILE.call("par")
+    end)
+  end, "Typeset its contents in a right aligned block (keeping margins).")
+
+  self:registerCommand("justified", function (_, content)
+    SILE.settings:temporarily(function ()
+      local lskip = SILE.settings:get("document.lskip") or SILE.nodefactory.glue()
+      local rskip = SILE.settings:get("document.rskip") or SILE.nodefactory.glue()
+      SILE.settings:set("document.lskip", SILE.nodefactory.glue(lskip.width.length))
+      SILE.settings:set("document.rskip", SILE.nodefactory.glue(rskip.width.length))
+      SILE.settings:set("document.spaceskip", nil)
+      -- HACK. This knows too much about parfillskip defaults...
+      -- (Which must be big, but smaller than infinity. Doh!)
+      SILE.settings:set("typesetter.parfillskip", SILE.nodefactory.glue("0pt plus 10000pt"))
+      SILE.process(content)
+      SILE.call("par")
+    end)
+  end, "Typeset its contents in a justified block (keeping margins).")
+
+  self:registerCommand("ragged", function (options, content)
+    -- Fairly dumb command for dubious compatibility
+    local l = SU.boolean(options.left, false)
+    local r = SU.boolean(options.right, false)
+    if l and r then
+      SILE.call("center", {}, content)
+    elseif r then
+      SILE.call("raggedleft", {}, content)
+    elseif l then
+      SILE.call("raggedright", {}, content)
+    else
+      SILE.call("justified", {}, content)
+    end
+  end)
+
+  self:registerCommand("rightalign", function (_, content)
+    -- Honestly, why would we need this stupid and redundant command?
+    -- It's not even a standard LaTeX command.
+    -- We don't even have leftalign to go with it.
+    SU.warn("rightalign ought to be deprecated. Use raggedleft instead.")
+    SILE.call("raggedleft", {}, content)
+  end)
 end
