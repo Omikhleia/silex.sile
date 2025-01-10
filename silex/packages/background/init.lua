4e2e5e05b56ac9c5e64fe5f4894b2d76f60216f7
Omikhleia <didier.willis@gmail.com>
2025-01-10 21:26:15 +0100

    chore(classes,packages): Styling and small updates from SILE 0.15.9

--- silex/packages/background/init.lua
+++ silex/packages/background/init.lua
@@ -6,52 +6,57 @@ package._name = "background"
 local background = {}
 
 local outputBackground = function ()
-  local pagea = SILE.getFrame("page")
-  local offset = SILE.documentState.bleed / 2
-  if type(background.bg) == "string" then
-    SILE.outputter:drawImage(background.bg,
-      pagea:left() - offset, pagea:top() - offset,
-      pagea:width() + 2 * offset, pagea:height() + 2 * offset)
-  elseif background.bg then
-    SILE.outputter:pushColor(background.bg)
-    SILE.outputter:drawRule(
-      pagea:left() - offset, pagea:top() - offset,
-      pagea:width() + 2 * offset, pagea:height() + 2 * offset)
-    SILE.outputter:popColor()
-  end
-  if not background.allpages then
-    background.bg = nil
-  end
+   local pagea = SILE.getFrame("page")
+   local offset = SILE.documentState.bleed / 2
+   if type(background.bg) == "string" then
+      SILE.outputter:drawImage(
+         background.bg,
+         pagea:left() - offset,
+         pagea:top() - offset,
+         pagea:width() + 2 * offset,
+         pagea:height() + 2 * offset
+      )
+   elseif background.bg then
+      SILE.outputter:pushColor(background.bg)
+      SILE.outputter:drawRule(
+         pagea:left() - offset,
+         pagea:top() - offset,
+         pagea:width() + 2 * offset,
+         pagea:height() + 2 * offset
+      )
+      SILE.outputter:popColor()
+   end
+   if not background.allpages then
+      background.bg = nil
+   end
 end
 
 function package:_init ()
-  base._init(self)
-  self.class:registerHook("newpage", outputBackground)
+   base._init(self)
+   self.class:registerHook("newpage", outputBackground)
 end
 
 function package:registerCommands ()
+   self:registerCommand("background", function (options, _)
+      if SU.boolean(options.disable, false) then
+         -- This option is certainly better than enforcing a white color.
+         background.bg = nil
+         return
+      end
 
-  self:registerCommand("background", function (options, _)
-    if SU.boolean(options.disable, false) then
-      -- This option is certainly better than enforcing a white color.
-      background.bg = nil
-      return
-    end
-
-    local allpages = SU.boolean(options.allpages, true)
-    background.allpages = allpages
-    local color = options.color and SILE.types.color(options.color)
-    local src = options.src
-    if src then
-      background.bg = src and SILE.resolveFile(src) or SU.error("Couldn't find file "..src)
-    elseif color then
-      background.bg = color
-    else
-      SU.error("background requires a color or an image src parameter")
-    end
-    outputBackground(SILE.scratch.background)
-  end, "Output a solid background color <color> or an image <src> on pages after initialization.")
-
+      local allpages = SU.boolean(options.allpages, true)
+      background.allpages = allpages
+      local color = options.color and SILE.types.color(options.color)
+      local src = options.src
+      if src then
+         background.bg = src and SILE.resolveFile(src) or SU.error("Couldn't find file " .. src)
+      elseif color then
+         background.bg = color
+      else
+         SU.error("background requires a color or an image src parameter")
+      end
+      outputBackground()
+   end, "Output a solid background color <color> or an image <src> on pages after initialization.")
 end
 
 package.documentation = [[
@@ -62,7 +67,7 @@ As its name implies, the \autodoc:package{background} package allows you to set 
 The package provides a \autodoc:command{\background} command which requires one of the following parameters:
 \begin{itemize}
 \item{\autodoc:parameter{color=<color specification>} sets the background of the current and all following pages to that color. The color specification has the same syntax as specified in the \autodoc:package{color} package.}
-\item{\autodoc:parameter{src=<file>} sets the backgound of the current and all following pages to the specified image. The latter will be scaled to the target dimension.}
+\item{\autodoc:parameter{src=<file>} sets the background of the current and all following pages to the specified image. The latter will be scaled to the target dimension.}
 \end{itemize}
 
 The background extends to the page trim area (“page bleed”) if the latter is defined.
